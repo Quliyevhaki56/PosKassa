b3a936afee57777057c768727190a81b55dfb349
quliyevhaki <quliyevhaki@gmail.com>
2025-10-24 04:27:12 +0400

    Updated MenuGrid.jsx

--- src/components/menu/MenuGrid.jsx
+++ src/components/menu/MenuGrid.jsx
@@ -75,13 +75,14 @@ export default function MenuGrid({
 						const isCategory = item.type === 'category' || item.type === 'subcategory';
 
 						return (
-							<button
+							<div>
+                  <button
 								key={`${item.type}-${item.data.id}`}
 								onClick={() => handleItemClick(item)}
 								className={`
 									relative rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow
 									flex flex-col items-center ${item.data.image ? null : "justify-center"} text-center
-									${isCategory ? getCategoryColor(index) + ' text-white' : 'bg-white border border-gray-200 hover:border-gray-300'} 
+									${isCategory ? getCategoryColor(index) + ' text-white' : 'bg-white border border-gray-200 hover:border-gray-300'}
 								`}
 								style={{ aspectRatio: '1/1' }}
 							>
@@ -103,6 +104,7 @@ export default function MenuGrid({
 									</div>
 								)}
 							</button>
+              </div>
 						);
 					})}
 				</div>
