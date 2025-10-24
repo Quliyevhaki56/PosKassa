1919b9acdfbe7b02bfc69fc0327e5dc803de1f8d
quliyevhaki <quliyevhaki@gmail.com>
2025-10-24 04:11:55 +0400

    Updated MenuGrid.jsx

--- src/components/menu/MenuGrid.jsx
+++ src/components/menu/MenuGrid.jsx
@@ -87,7 +87,7 @@ export default function MenuGrid({
 							>
 								{item.data.image_url && !isCategory && (
 									<img
-										src={item.data.image_url} 
+										src={item.data.image}
 										alt={item.data.name}
 										className='w-full h-24 object-cover mb-2 rounded'
 									/>
