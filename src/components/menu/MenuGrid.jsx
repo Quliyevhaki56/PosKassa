f2fa1cb9507fa48b7b7b41bd27189c3bfe8f354e
quliyevhaki <quliyevhaki@gmail.com>
2025-10-24 04:24:10 +0400

    Updated MenuGrid.jsx

--- src/components/menu/MenuGrid.jsx
+++ src/components/menu/MenuGrid.jsx
@@ -79,11 +79,10 @@ export default function MenuGrid({
 								key={`${item.type}-${item.data.id}`}
 								onClick={() => handleItemClick(item)}
 								className={`
-									relative rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow
+									min-w-200 relative rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow
 									flex flex-col items-center ${item.data.image ? null : "justify-center"} text-center
 									${isCategory ? getCategoryColor(index) + ' text-white' : 'bg-white border border-gray-200 hover:border-gray-300'}
 								`}
-								style={{ aspectRatio: '1/1' }}
 							>
 								{item.data.image && !isCategory && (
 									<img
